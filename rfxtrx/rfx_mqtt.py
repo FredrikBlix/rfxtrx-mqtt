42e48f0fd0a85f9a636ca16c00e41e5c6ce92eba
Fredrik Blix <fredrik.blix@gmail.com>
2025-02-25 15:17:45 +0100

    Remove unused imports from RFXtrx modules and update pylint configuration

--- rfxtrx/rfx_mqtt.py
+++ rfxtrx/rfx_mqtt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import argparse
-import json
 from rfxtrx.rfxtrx import RFXtrx
 from rfxtrx.mqtt_handler import MQTTHandler
 
--- rfxtrx/rfxtrx.py
+++ rfxtrx/rfxtrx.py
@@ -1,5 +1,4 @@
 import serial
-import json
 import time
 import binascii
 
